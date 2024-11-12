17767c4f2fa6afebe5b01836dd74c10eed3cde48
dann <austindan168@gmail.com>
2024-11-12 16:43:01 +0300

    search

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -5,18 +5,21 @@ import Search from "./Search";
 
 function PlantPage() {
   const [plants, setPlants] = useState([])
+  const [searchPlant,setSearchPlant] = useState("")
 
   useEffect(() => {
     fetch("http://localhost:6001/plants")
     .then(res => res.json())
     .then(data => {setPlants(data)})
   },[])
-  
+   
+   const displayPlant = plants.filter((plante) => `${plants.name}`.toLowerCase().includes(searchPlant.toLowerCase()))
+
   return (
     <main>
       <NewPlantForm plants={plants} setPlants={setPlants}/>
-      <Search />
-      <PlantList plants={plants} setPlants={setPlants} />
+      <Search onSearch={setSearchPlant}/>
+      <PlantList plants={displayPlant} setPlants={setPlants} />
     </main>
   );
 }
